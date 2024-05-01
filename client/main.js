e50a3c52fefea0ed375378f99c7792c3b155ef23
JacobBarber1 <jacobbarber1@hotmail.co.uk>
2024-05-01 14:37:41 +0100

    set up project

---
+++ client/main.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.getElementById('time').innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+setInterval(updateTime, 1000);
